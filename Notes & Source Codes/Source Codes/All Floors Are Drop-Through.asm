f0f16fa4df8ea2885d6b120c01884b31ecfc946c
Durgan <72322229+DRGN-DRC@users.noreply.github.com>
2023-02-11 20:06:18 -0800

    Fixed up source code for All Floors Are Drop-Through

--- Notes & Source Codes/Source Codes/All Floors Are Drop-Through.asm
+++ Notes & Source Codes/Source Codes/All Floors Are Drop-Through.asm
@@ -4,13 +4,35 @@ All floors can be dropped through (and ceilings can be passed through) like stan
 <https://smashboards.com/threads/all-floors-are-drop-through.513411/>
 [DRGN]
 Revision ---- DOL Offset ---- Hex to Replace ---------- ASM Code -
-## Original code:
+
+## The original verion of this code:
 ## NTSC 1.02 --- 0x8004CBD4 ---- 546305EE -> 38600100 # rlwinm r3,r3,0,23,23(00000100) -> li r3, 0x100
 ## ------------- 0x8004FD24 ---- AB240006 -> 3B200000 # lha r25, 0x0006 (r4) -> li r25, 0
 
-# The original code modifies DataOffset_CheckIfOnDropThroughPlatform and ECB_StoreCeilingID+Type.
-# This variation instead makes this change while in the debug menu, using the line menu item's 'target function'.
-# Conveniently, the current value is updated before a menu item's target function is executed.
+## The original code modifies DataOffset_CheckIfOnDropThroughPlatform and ECB_StoreCeilingID+Type.
+## This variation instead makes this change while in the debug menu, using the line menu item's 'target function'.
+## Conveniently, the current value is updated before a menu item's target function is executed.
+
+NTSC 1.02 --- 0x80393BAC ----
+
+387A0090 93810020
+C862EF60 4CC63242
+93810028 C8210020
+C8010028 EC211028
+93810018 EC001028
+C082EF50 C8410018
+EC210024 C002EF54
+EC421828 EC4400B2
+EC420824 EC420032
+4BFB1AB5 7FE3FB78
+BB210034 8001005C
+CBE10050 38210058
+7C0803A6 4E800020
+
+ -> 
+
+# This is being stored at the end of Area 5 of USB/MCC region. It's stored as a static overwrite 
+# rather than an injected someplace so it can be referenced from a debug menu's line item function.
 
 .macro SetWord address, value
   lis r15, \address@h		# Load the address to modify
@@ -37,3 +59,5 @@ SetWord 0x8004FD24, 0xAB240006
 
 END:
 blr
+
+------------- 0x802288E8 ---- 54630529 -> 00000000 # Debug Menu flag
